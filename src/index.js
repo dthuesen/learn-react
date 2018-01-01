9d7b122263e7ccee2e659f2de2d4b330241d5cb3
Detlef von der Thuesen <dthuesen@gmx.de>
2018-01-01 18:11:37 +0100

    add YTSearch() method to state of index.js and prepare video_list.js component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDg3jFevSmIahIfaWSgg4suz04I3bXaWuI';
 
-// 1) Create a new component. This component should produce some HTML
-/**
- * Currently this is a functional component because it simple is a component build by a function
+
+
+/**  
+ * In React the most top level component is responsive for fetching data for the downwards data flow.
+ * This is currently at the most top level. So this component will fetch the data for all the child components.
  */
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            videos: [],
+        }
+
+        YTSearch({ key: API_KEY, searchTerm: 'surfboards'}, videos => {
+            this.setState({ videos }); // same as '{ videos: videos }'
+        })
+    }
+
+    render()  {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    };
 }
 
-// 2) Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
 
